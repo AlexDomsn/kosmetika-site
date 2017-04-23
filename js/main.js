f16efff304e88d63e31fec425c3059a32a755dc5
AlexDomson <alex@4ats.ru>
2017-04-23 11:00:56 +0300

    coffe_1st_change

--- js/main.js
+++ js/main.js
@@ -1,32 +1,15 @@
-$(document).ready(function () {
-    $('.Carousel_slick').slick({
-        dots: true,
-        // dotsClass: 'Slick_dots',
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        prevArrow: '<a href="#" class="arrow_cont prev"><img src="images/arrow.png" alt="" class="customer_arrow" /></a>',
-        nextArrow: '<a href="#" class="arrow_cont next"><img src="images/right-arrow.png" alt=""  class="customer_arrow" /></a>'
-
-    });
-
-    $("form").submit(function () {
-        var a = $(this);
-        $.ajax({type: "POST", url: "mail.php", data: a.serialize()}).done(function () {
-            setTimeout(function () {
-                document.querySelector(".modal-open").checked = !0
-            }, 1E3);
-            setTimeout(function () {
-                $(".modal-open").attr("checked", !1).trigger("reset")
-            }, 5E3)
-        });
-        return !1
-    });
-    $(".close_btn").click(function () {
-        $(".modal-open").attr("checked", !1)
-    });
-    setTimeout(function () {
-        $(".modal-open").attr("checked", !1)
-    }, 5E3);
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: false,
+    focusOnSelect: true
 });
